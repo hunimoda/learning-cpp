0bf2fdf961b81dc7001ed6d6ad7c444a32f401fd
hunimoda <hunimoda@gmail.com>
2022-04-16 09:50:40 +0900

    Udemy: first c++ program

---
+++ udemy-cpp/Section4/FirstProgram/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int favorite_number;
+
+    std::cout << "Enter your favorite number (0 ~ 100): ";
+    std::cin >> favorite_number;
+
+    std::cout << "Amazing!! That's my favorite number, too!" << std::endl;
+    
+    return 0;
+}

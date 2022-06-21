bb587be9acd1a1172c83c9a7f99990755ddbf701
Hunimoda <hunimoda@gmail.com>
2022-06-21 17:53:42 +0900

    add basic c++ code: include guard, hello world

---
+++ professional-cpp/Chapter_01/header.h
@@ -0,0 +1,5 @@
+//#pragma once  // google code style says not to use this
+#ifndef MY_HEADER_H_
+#define MY_HEADER_H_
+
+#endif
---
+++ professional-cpp/Chapter_01/hello_world.cc
@@ -0,0 +1,7 @@
+// import <iostream>  // import is not supported yet
+#include <iostream>  // use #include for a while
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  // std::endl -> flushes buffer compared to '\n': may have performance issue
+}
